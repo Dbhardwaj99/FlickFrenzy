68494ace29bbef9a881c2e40c24c191b0fcd61b6
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2023-12-27 14:13:05 +0530

    Initial Commit

---
+++ FlickFrenzy/FlickFrenzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickFrenzyApp.swift
+//  FlickFrenzy
+//
+//  Created by Divyansh Bhardwaj on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FlickFrenzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
